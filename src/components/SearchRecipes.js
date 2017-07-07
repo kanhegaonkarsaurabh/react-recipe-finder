d5b610349f177eee5f94c5ec69f024de756aefc0
Saurabh Kanhegaonkar <saurabh.kanhegaonkar@gmail.com>
2017-07-07 12:41:26 +0530

    Form added and user inputs stored locally

---
+++ src/components/SearchRecipes.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { Form, FormGroup, FormControl, ControlLabel, Button } from 'react-bootstrap';
+
+class SearchRecipes extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      ingredients : '',
+      dish : ''
+    };
+  }
+
+  search() {
+    let { ingredients, dish } = this.state;
+    const url = `http://recipepuppy.com/api/?i=${ingredients}&q=${dish}`;
+    console.log('url', url);
+  }
+
+
+  render() {
+    return (
+      <Form inline>
+        <FormGroup>
+          <ControlLabel>Ingredients</ControlLabel>
+          {'   '}
+          <FormControl type = "text"
+            placeholder = "onion, fish"
+            onChange = {(event) => this.setState({
+              ingredients : event.target.value
+            })}
+            />
+
+
+        </FormGroup>
+        {' '}
+        <FormGroup>
+          <ControlLabel>Dish</ControlLabel>
+          {' '}
+          <FormControl
+          type = "text"
+          placeholder = "pasta"
+          onChange = {(event) => this.setState({
+            dish : event.target.value
+          })}
+          />
+        </FormGroup>
+        {' '}
+        <Button onClick = {() => this.search()}>
+          Submit
+        </Button>
+
+
+
+      </Form>
+    )
+  }
+
+}
+
+export default SearchRecipes;
